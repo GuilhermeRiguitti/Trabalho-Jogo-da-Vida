6492e6662fe26b5a0435743b59a30221bf0370b9
SavioCarlos <110927106+SavioCarlos@users.noreply.github.com>
2023-03-14 23:16:10 -0300

    Main

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
     jogo.imprimirMatriz();
     Scanner scanner = new Scanner(System.in);
     
-    // O programa solicita a entrada do usuário para decidir quando sair, atualiza e imprime a matriz repetidamente até que o usuário opte por sair.
+    // O programa solicita a entrada do usuário para decidir quando sair, atualiza e imprime a matriz repetidamente até que o usuário opte por sair .
     while (true) {
       System.out.println("Digite 'f' para sair ou enter para continuar: ");
       String input = scanner.nextLine();
